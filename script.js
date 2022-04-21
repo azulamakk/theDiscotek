f0c57cb782e9f603d16118a94d63cdf147eb137c
Azul Makk <azulamakk@gmail.com>
2022-04-21 11:54:14 -0300

    parseInt, +promp, alert

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-let nombre, apellido, fecnac
-let nombreCompleto = nombre + " " + apellido 
+let nombre, apellido, fecnac;
+let nombreCompleto = nombre + " " + apellido; 
 
-//Carga de datos
-nombre = prompt("Ingresa tu nombre")
-apellido = prompt("Ingresa tu Apellido")
-fecnac = prompt("Ingrese fecha de nacimiento")
+//Carga de datos - Conversion a str
+nombre = prompt("Ingresa tu nombre");
+apellido = prompt("Ingresa tu Apellido");
 
 // Msg 4 Dev
-console.log("Variables en JS")
-console.log("El nombre completo es "+nombreCompleto)
+console.log("Variables en JS");
+console.log("El nombre completo es "+nombreCompleto);
+
+let anioNacimiento = parseInt(prompt("Ingrese anio de nacimiento")); //parse to str
+console.log(nombre + " nacio en " + anioNacimiento);
+
+let edadUsuario = 2022 - anioNacimiento;
+console.log("La edad del usuario es: "+edadUsuario);
+
+// Alert - Salida de datos
+alert("Usuario activo: "+nombre)
