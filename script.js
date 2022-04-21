3027966445d7e1aac4536b6d2c1a3de88c9ab2cd
Azul Makk <azulamakk@gmail.com>
2022-04-21 11:35:35 -0300

    intro a js: prompt, log, var incic, link html, msg 4 dev

---
+++ script.js
@@ -0,0 +1,11 @@
+let nombre, apellido, fecnac
+let nombreCompleto = nombre + " " + apellido 
+
+//Carga de datos
+nombre = prompt("Ingresa tu nombre")
+apellido = prompt("Ingresa tu Apellido")
+fecnac = prompt("Ingrese fecha de nacimiento")
+
+// Msg 4 Dev
+console.log("Variables en JS")
+console.log("El nombre completo es "+nombreCompleto)
