5b6ab4816e5aaa83c512dcf481b55ae30a529498
Azul Makk <azulamakk@gmail.com>
2022-05-03 11:15:01 -0300

    Inventarios & Control de errores

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
 // Su objetivo principal es que un empleado al ingresar al turno sea registrado y mas adelante se puedan registar las materias primas utilizadas
 
 let nombreEmpleado, apellidoEmpleado, fecnacEmpleado, usuarioEmpleado, contrasenaEmpleado;
-let nombreCompletoEmpleado = nombre + " " + apellido; 
 let horarioLlegada, diaTrabajado
 
+usuarioEmpleado="amakk"
+contrasenaEmpleado="Canelones123"
 //Carga de datos - Conversion a str
+usuarioIngresado = prompt("¿Cual es su nombre de usuario?");
+contrasenaIngresado = prompt("Ingrese su contraseña")
 
 for (let i=1;i=3;i++){
-    if (i=1){
-        usuarioIngresado = prompt("¿Cual es su nombre de usuario?");
-        contrasenaIngresado = prompt("Ingrese su contraseña")
-    } else {
+    if ((i=1)&&(contrasenaEmpleado != contrasenaIngresado)){
         contrasenaIngresado = prompt("La contraseña ingresada es incorrecta. Por favor ingresela nuevamente")
     }
     if (contrasenaEmpleado=contrasenaIngresado){
+        alert("Usuario y contraseña son correctos. Enhorabuena! Usted ha iniciado sesión")
         break;
     }
     if ((i=3) && (contrasenaEmpleado != contrasenaIngresado)){
@@ -23,16 +24,17 @@ for (let i=1;i=3;i++){
     }
 }
 
-// Msg 4 Dev
-console.log("El nombre completo es "+nombreCompleto);
-
-let anioNacimiento = parseInt(prompt("Ingrese anio de nacimiento")); //parse to str
-console.log(nombreCompleto + " nacio en " + anioNacimiento);
-
-let edadUsuario = 2022 - anioNacimiento;
+anioNacimientoUsuario="2002"
+let edadUsuario = 2022 - anioNacimientoUsuario;
 console.log("La edad del usuario es: "+edadUsuario);
 
+// Usuarios conectados
+console.log("Usuario activo: "+usuarioEmpleado)
+console.log("Ultima vez consultado: "+new Date().toLocaleTimeString())
 
-// Alert - Salida de datos
-alert("Usuario activo: "+usuarioEmpleado)
+//Control de inventarios
+let articulo, cantUnidades, totalUnidades
+articulo = parseInt(prompt("Ingrese el codigo de articulo que utilizó en su turno"))
+cantUnidades = parseInt(prompt("¿Cuantos componentes ha utilizado?"))
 
+totalUnidades = totalUnidades - cantUnidades
