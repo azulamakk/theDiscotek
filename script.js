7cc668eb7d31f226a801f5d54c997371dd8f9c26
Azul Makk <azulamakk@gmail.com>
2022-05-16 12:03:37 -0300

    user fix

--- script.js
+++ script.js
@@ -4,10 +4,9 @@
 let nombreEmpleado, apellidoEmpleado, fecnacEmpleado, usuarioEmpleado, contrasenaEmpleado;
 let horarioLlegada, diaTrabajado, anioIngresoEmpleado
 //const empleados = []
-const empleados = [
-    {
-        nombre:"Jorge",
-        apellido:"Makk", 
+const empleados = [{
+        nombre: "Jorge",
+        apellido: "Makk",
         anioNacimiento: 1970,
         aniosAntiguedad: 32,
         anioIngresoEmpleado: 2002,
@@ -15,8 +14,8 @@ const empleados = [
         contrasena: "Milanesa123"
     },
     {
-        nombre:"Jose",
-        apellido:"Makk", 
+        nombre: "Jose",
+        apellido: "Makk",
         anioNacimiento: 1940,
         aniosAntiguedad: 64,
         anioIngresoEmpleado: 1975,
@@ -24,8 +23,8 @@ const empleados = [
         contrasena: "Paty1"
     },
     {
-        nombre:"Elizabeth",
-        apellido:"Llanos", 
+        nombre: "Elizabeth",
+        apellido: "Llanos",
         anioNacimiento: 1975,
         aniosAntiguedad: 28,
         anioIngresoEmpleado: 2020,
@@ -36,31 +35,31 @@ const empleados = [
 //Carga de datos - Conversion a str
 usuarioIngresado = prompt("¿Cual es su nombre de usuario?");
 contrasenaIngresado = prompt("Ingrese su contraseña")
-for (let m=1; m=3;i++){
+let edadUsuario
+for (let m = 1; m = 3; i++) {
     let posicionEnObjeto
-    posicionEnObjeto = empleados.indexOf(usuario=usuarioEmpleado)
-    contrasenaEmpleado = empleados[posicionEnObjeto+1].contrasena
-    for (let i=1;i=3;i++){
-        if ((i=1)&&(contrasenaEmpleado != contrasenaIngresado)){
+    posicionEnObjeto = empleados.indexOf(usuario = usuarioEmpleado)
+    contrasenaEmpleado = empleados[posicionEnObjeto].contrasena
+    edadUsuario = 2022 - empleados[posicionEnObjeto].anioNacimiento
+    for (let i = 1; i = 3; i++) {
+        if ((i = 1) && (contrasenaEmpleado != contrasenaIngresado)) {
             contrasenaIngresado = prompt("La contraseña ingresada es incorrecta. Por favor ingresela nuevamente")
         }
-        if (contrasenaEmpleado=contrasenaIngresado){
+        if (contrasenaEmpleado = contrasenaIngresado) {
             alert("Usuario y contraseña son correctos. Enhorabuena! Usted ha iniciado sesión")
             break;
         }
-        if ((i=3) && (contrasenaEmpleado != contrasenaIngresado)){
+        if ((i = 3) && (contrasenaEmpleado != contrasenaIngresado)) {
             alert("Ups! Ya ingresado 3 intentos insatisfactorios. Por favor contactese con el administrados para reestablecer su contraseña")
             break;
         }
     }
 }
-anioNacimientoUsuario="2002"
-let edadUsuario = 2022 - anioNacimientoUsuario;
-console.log("La edad del usuario es: "+edadUsuario);
+console.log("La edad del usuario es: " + edadUsuario);
 
 // Usuarios conectados
-console.log("Usuario activo: "+usuarioEmpleado)
-console.log("Ultima vez consultado: "+new Date().toLocaleTimeString())
+console.log("Usuario activo: " + usuarioEmpleado)
+console.log("Ultima vez consultado: " + new Date().toLocaleTimeString())
 
 //Control de inventarios
 let articuloIngresado, cantUnidadesUsadas, totalUnidades
@@ -72,26 +71,26 @@ cantUnidadesUsadas = parseInt(prompt("¿Cuantos componentes ha utilizado?"))
 let cantUnidadesIngresadas = parseInt(prompt("¿Cuantas unidades se han ingresado al warehouse?"))
 
 //Idealmente en un futuro, existiria una lista de codigos de articulos cargados
-let articulo=inventarios
+let articulo = inventarios
+
 function cantArticulosTotal(articuloIngresado) {
-    while (articuloIngresado==articulo){
+    while (articuloIngresado == articulo) {
         totalUnidades = totalUnidades - cantUnidadesUsadas
         totalUnidades = totalUnidades + cantUnidadesIngresadas
         return totalUnidades
     }
-    for (i=1;i=5;i++){
-        if (articuloIngresado!=articulo){
+    for (i = 1; i = 5; i++) {
+        if (articuloIngresado != articulo) {
             alert("El codigo de articulo ingresado no coincide con el inventario. Intente de nuevo por favor")
             articuloIngresado = prompt("Codigo articulo buscado")
         }
-        if(i>4){
+        if (i > 4) {
             alert("Se recomienda revisar los manuales de codigos de producto. Intente mas tarde")
         }
     }
 }
-console.log("Actualmente hay "+totalUnidades+" unidades disponibles de articulo "+articulo);
+console.log("Actualmente hay " + totalUnidades + " unidades disponibles de articulo " + articulo);
 let nombreArt, porcentajeInflacionMes
-
 class inventarioTotal {
     constructor(id, nombre, precio) {
         this.id = parseInt(articulo);
@@ -99,8 +98,8 @@ class inventarioTotal {
         this.disponible = true;
         this.cantStock = parseInt(totalUnidades);
     }
-    actualizacionInflacion(){
-        this.precio = this.precio * (1+porcentajeInflacionMes)
+    actualizacionInflacion() {
+        this.precio = this.precio * (1 + porcentajeInflacionMes)
     }
 }
 const inventarios = [];
