3a2eba3c404e59ab172c0741a11bc65b40404835
Azul Makk <azulamakk@gmail.com>
2022-05-16 11:51:35 -0300

    Intro Arrays

--- script.js
+++ script.js
@@ -2,28 +2,58 @@
 // Su objetivo principal es que un empleado al ingresar al turno sea registrado y mas adelante se puedan registar las materias primas utilizadas
 
 let nombreEmpleado, apellidoEmpleado, fecnacEmpleado, usuarioEmpleado, contrasenaEmpleado;
-let horarioLlegada, diaTrabajado
-
-usuarioEmpleado="amakk"
-contrasenaEmpleado="Canelones123"
+let horarioLlegada, diaTrabajado, anioIngresoEmpleado
+//const empleados = []
+const empleados = [
+    {
+        nombre:"Jorge",
+        apellido:"Makk", 
+        anioNacimiento: 1970,
+        aniosAntiguedad: 32,
+        anioIngresoEmpleado: 2002,
+        usuario: "jamakk",
+        contrasena: "Milanesa123"
+    },
+    {
+        nombre:"Jose",
+        apellido:"Makk", 
+        anioNacimiento: 1940,
+        aniosAntiguedad: 64,
+        anioIngresoEmpleado: 1975,
+        usuario: "jmakk",
+        contrasena: "Paty1"
+    },
+    {
+        nombre:"Elizabeth",
+        apellido:"Llanos", 
+        anioNacimiento: 1975,
+        aniosAntiguedad: 28,
+        anioIngresoEmpleado: 2020,
+        usuario: "ellanos",
+        contrasena: "Canelones"
+    }
+]
 //Carga de datos - Conversion a str
 usuarioIngresado = prompt("¿Cual es su nombre de usuario?");
 contrasenaIngresado = prompt("Ingrese su contraseña")
-
-for (let i=1;i=3;i++){
-    if ((i=1)&&(contrasenaEmpleado != contrasenaIngresado)){
-        contrasenaIngresado = prompt("La contraseña ingresada es incorrecta. Por favor ingresela nuevamente")
-    }
-    if (contrasenaEmpleado=contrasenaIngresado){
-        alert("Usuario y contraseña son correctos. Enhorabuena! Usted ha iniciado sesión")
-        break;
-    }
-    if ((i=3) && (contrasenaEmpleado != contrasenaIngresado)){
-        alert("Ups! Ya ingresado 3 intentos insatisfactorios. Por favor contactese con el administrados para reestablecer su contraseña")
-        break;
+for (let m=1; m=3;i++){
+    let posicionEnObjeto
+    posicionEnObjeto = empleados.indexOf(usuario=usuarioEmpleado)
+    contrasenaEmpleado = empleados[posicionEnObjeto+1].contrasena
+    for (let i=1;i=3;i++){
+        if ((i=1)&&(contrasenaEmpleado != contrasenaIngresado)){
+            contrasenaIngresado = prompt("La contraseña ingresada es incorrecta. Por favor ingresela nuevamente")
+        }
+        if (contrasenaEmpleado=contrasenaIngresado){
+            alert("Usuario y contraseña son correctos. Enhorabuena! Usted ha iniciado sesión")
+            break;
+        }
+        if ((i=3) && (contrasenaEmpleado != contrasenaIngresado)){
+            alert("Ups! Ya ingresado 3 intentos insatisfactorios. Por favor contactese con el administrados para reestablecer su contraseña")
+            break;
+        }
     }
 }
-
 anioNacimientoUsuario="2002"
 let edadUsuario = 2022 - anioNacimientoUsuario;
 console.log("La edad del usuario es: "+edadUsuario);
@@ -42,7 +72,7 @@ cantUnidadesUsadas = parseInt(prompt("¿Cuantos componentes ha utilizado?"))
 let cantUnidadesIngresadas = parseInt(prompt("¿Cuantas unidades se han ingresado al warehouse?"))
 
 //Idealmente en un futuro, existiria una lista de codigos de articulos cargados
-let articulo=1020
+let articulo=inventarios
 function cantArticulosTotal(articuloIngresado) {
     while (articuloIngresado==articulo){
         totalUnidades = totalUnidades - cantUnidadesUsadas
@@ -60,4 +90,18 @@ function cantArticulosTotal(articuloIngresado) {
     }
 }
 console.log("Actualmente hay "+totalUnidades+" unidades disponibles de articulo "+articulo);
+let nombreArt
+
+class inventarioTotal {
+    constructor(id, nombre, precio) {
+        this.id = parseInt(articulo);
+        this.nombre = nombreArt.toUpperCase();
+        this.disponible = true;
+        this.cantStock = parseInt(totalUnidades);
+    }
+}
+const inventarios = [];
+inventarios.push(new inventarioTotal("1010", "Valvula Rosca", 45));
+inventarios.push(new inventarioTotal("1020", "Valvula Presion", 80));
+inventarios.push(new inventarioTotal("3030", "Cierre hidraulico", 200));
 
