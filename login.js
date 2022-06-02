9657134ee5b4b52fd79b93494d440e62eb2f05a1
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-06-02 10:43:49 -0300

    cambios varios

---
+++ login.js
@@ -0,0 +1,29 @@
+//Declaracion de empleados
+const empleados = [{
+    nombre: "Jorge",
+    apellido: "Makk",
+    anioNacimiento: 1970,
+    aniosAntiguedad: 32,
+    anioIngresoEmpleado: 2002,
+    usuario: "jamakk",
+    contrasena: "Milanesa123"
+},
+{
+    nombre: "Jose",
+    apellido: "Makk",
+    anioNacimiento: 1940,
+    aniosAntiguedad: 64,
+    anioIngresoEmpleado: 1975,
+    usuario: "jmakk",
+    contrasena: "Paty1"
+},
+{
+    nombre: "Elizabeth",
+    apellido: "Llanos",
+    anioNacimiento: 1975,
+    aniosAntiguedad: 28,
+    anioIngresoEmpleado: 2020,
+    usuario: "ellanos",
+    contrasena: "Canelones"
+}
+]
--- script.js
+++ script.js
@@ -143,38 +143,5 @@ function activarDark(){
 //Control de inventarios
 
 
-//Ingreso de unidades
-//Registro de unidades en caso de re-stock. Control a cargo del empleado de turno.
 
-//Idealmente en un futuro, existiria una lista de codigos de articulos cargados
-
-//Declaracion de empleados
-const empleados = [{
-    nombre: "Jorge",
-    apellido: "Makk",
-    anioNacimiento: 1970,
-    aniosAntiguedad: 32,
-    anioIngresoEmpleado: 2002,
-    usuario: "jamakk",
-    contrasena: "Milanesa123"
-},
-{
-    nombre: "Jose",
-    apellido: "Makk",
-    anioNacimiento: 1940,
-    aniosAntiguedad: 64,
-    anioIngresoEmpleado: 1975,
-    usuario: "jmakk",
-    contrasena: "Paty1"
-},
-{
-    nombre: "Elizabeth",
-    apellido: "Llanos",
-    anioNacimiento: 1975,
-    aniosAntiguedad: 28,
-    anioIngresoEmpleado: 2020,
-    usuario: "ellanos",
-    contrasena: "Canelones"
-}
-]
-
+sessionStorage.clear()
