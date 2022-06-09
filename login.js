ab0e16b4cfea7c424c9eb8c4136496375596aad0
azulamakk <92961593+azulamakk@users.noreply.github.com>
2022-06-09 01:39:17 -0300

    log in update

--- login.js
+++ login.js
@@ -25,20 +25,51 @@ const empleados = [{
     anioIngresoEmpleado: 2020,
     usuario: "ellanos",
     contrasena: "Canelones"
+}]
+
+
+let registrar = document.getElementById("btnCargaFinalizada")
+registrar.innerText = "Ingresar";
+
+var jwt = localStorage.getItem("jwt");
+if (jwt != null) {
+  window.location.href = "index.html"
 }
-]
-let usuario
-function validar() {
-    var usuario = document.getElementById("usuario");
-    var constrasenia = document.getElementById("contrasenia");
-    if(usuario==empleados.usuario){
-        for (empleado of empleados){
-            if (empleado.constrasena == empleados.constrasenia){
-                alert("Usuario y contraseña validos")
-            }
-            else{
-                alert("Usuario o contraseña invalidos. Intente nuevamente")
-            }
-        }
+
+function login() {
+  const usuario = document.getElementById("usuario").value;
+  const contrasenia = document.getElementById("contrasenia").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://www.mecallapi.com/api/login");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.send(JSON.stringify({
+    "usuario": usuario,
+    "contrasenia": contrasenia
+  }));
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      if (objects['status'] == 'ok') {
+        localStorage.setItem("jwt", objects['accessToken']);
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "index.html";
+          }
+        });
+      } else {
+        Swal.fire({
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
     }
+  };
+  return false;
 }
